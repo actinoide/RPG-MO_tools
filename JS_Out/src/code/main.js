f007368bb0e3ae1f7769f5aa0cd08cc8d85b041c
actinoide <das0schaf@gmx.de>
2026-05-30 20:56:09 +0200

    refactored index again to avoid loading readme with pages

--- JS_Out/src/code/main.js
+++ JS_Out/src/code/main.js
@@ -9,7 +9,7 @@ function createWindow() {
             preload: path.join(__dirname, 'preload.js')
         }
     });
-    win.loadFile('./index.html');
+    win.loadFile('./docs/index.html');
 }
 app.whenReady().then(() => {
     createWindow();
